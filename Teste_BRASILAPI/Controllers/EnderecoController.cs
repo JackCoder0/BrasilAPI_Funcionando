a8406eabeb0dbb2dd1250cabbb3b2acbc43ceaa6
Jackson A Silva <jacksonxcontato13@gmail.com>
2024-05-27 11:55:42 -0300

    Add project files.

---
+++ Teste_BRASILAPI/Controllers/EnderecoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Teste_BRASILAPI.Interfaces;
+using Teste_BRASILAPI.Services;
+
+namespace Teste_BRASILAPI.Controllers
+{
+    [ApiController]
+    [Route("{controller}")]
+    public class EnderecoController : Controller
+    {
+        private readonly EnderecoService _enderecoService;
+
+        public EnderecoController(EnderecoService enderecoService)
+        {
+            _enderecoService = enderecoService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet("{action}")] 
+        public async Task<IActionResult> EnderecoInfo(string cep)
+        {
+            var response = await _enderecoService.BuscarEndereco(cep);
+
+           return View(response);
+        }
+
+  
+    }
+}
