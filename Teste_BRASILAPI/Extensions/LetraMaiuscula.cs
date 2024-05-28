e11032e5bad8b69217536b89a17ef8d87908cb68
Jackson A Silva <jacksonxcontato13@gmail.com>
2024-05-28 08:52:25 -0300

    LetraMaiuscula?

---
+++ Teste_BRASILAPI/Extensions/LetraMaiuscula.cs
@@ -0,0 +1,12 @@
+﻿namespace Teste_BRASILAPI.Extensions;
+
+public static class LetraMaiuscula
+{
+    public static string PrimeiraLetra(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return input;
+
+        return char.ToUpper(input[0]) + input.Substring(1).ToLower();
+    }
+}
